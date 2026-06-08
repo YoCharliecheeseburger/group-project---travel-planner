6ff63220ee3206342d69779aaa773769b141bebc
YoCharliecheeseburger <170423299+YoCharliecheeseburger@users.noreply.github.com>
2026-06-08 10:47:25 +0300

    backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+const port = process.env.PORT || 3000;
+
+app.use(cors({origin: "*"}));
+
 app.use(express.json());
 
 let trips = [];
